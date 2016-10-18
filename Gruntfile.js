e3353664ae4cdd74ec254e3d72dbcbee24e0f15d
Tobias Gruetzmacher <Tobias.Gruetzmacher@inform-software.com>
2016-10-18 16:14:46 +0200

    Use common code from jenkins-core-theme.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,124 @@
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
 
+    var pkg = grunt.file.readJSON('package.json');
+
+    var lessFiles = {
+        "dist/neo-light.css": "less/style.less",
+        "dist/neo-dark.css": "less/dark.less",
+    };
+
+    var replaceFiles = [
+        {src: ['dist/neo-light.css'], dest: 'dist/neo-light.css'},
+        {src: ['dist/neo-dark.css'], dest: 'dist/neo-dark.css'}
+    ];
+
+    var cssMinFiles = {
+        'dist/neo-light.css': ['dist/neo-light.css'],
+        'dist/neo-dark.css': ['dist/neo-dark.css']
+    };
+
+
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
         clean: {
             dist: {
-                src: ["dist/"]
+                src: ["dist/*"]
             }
         },
+
+        imagemin: {
+            dynamic: {
+                options: {
+                    svgoPlugins: [{
+                        removeDoctype: true,
+                        removeXMLProcInst: true,
+                        removeComments: true,
+                        removeMetadata: true,
+                        removeTitle: true,
+                        removeDesc: true,
+                        removeUselessDefs: true,
+                        removeEditorsNSData: true,
+                        removeEmptyAttrs: true,
+                        removeHiddenElems: true,
+                        removeEmptyText: true,
+                        removeEmptyContainers: true,
+                        removeViewBox: true,
+                        cleanUpEnableBackground: true,
+                        minifyStyles: true,
+                        convertStyleToAttrs: true,
+                        convertColors: true,
+                        convertPathData: true,
+                        convertTransform: true,
+                        removeUnknownsAndDefaults: true,
+                        removeNonInheritableGroupAttrs: true,
+                        removeUselessStrokeAndFill: true,
+                        removeUnusedNS: true,
+                        cleanupIDs: true,
+                        cleanupNumericValues: true,
+                        moveElemsAttrsToGroup: true,
+                        moveGroupAttrsToElems: true,
+                        collapseGroups: true,
+                        removeRasterImages: true,
+                        mergePaths: true,
+                        convertShapeToPath: true,
+                        sortAttrs: true,
+                        transformsWithOnePath: true,
+                        removeDimensions: true,
+                        removeAttrs: true,
+                        addClassesToSVGElement: true,
+                        removeStyleElement: true
+                    }]
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules/jenkins-core-theme/images/',
+                    src: ['**/*.svg'],
+                    dest: 'node_modules/jenkins-core-theme/images/'
+                }]
+            }
+
+        },
+
         less: {
             dist: {
-                files: {
-                    "dist/neo-light.css": "less/style.less",
-                    "dist/neo-dark.css": "less/dark.less",
-                }
+                files: lessFiles
             }
         },
+
+        replace: {
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'version',
+                            replacement: pkg.version
+                        }
+                    ]
+                },
+                files: replaceFiles
+            }
+        },
+
+        cssmin: {
+            minify: {
+                files: cssMinFiles
+            }
+        },
+
+        postcss: {
+            options: {
+                map: false,
+                processors: [
+                    require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+                    require('postcss-encode-base64-inlined-images'),
+                    require('cssnano')() // minify the result
+                ]
+            },
+            dist: {
+                src: 'dist/material*.css'
+            }
+        },
+
         imageEmbed: {
             light: {
                 src: ["dist/neo-light.css"],
@@ -32,29 +135,16 @@ module.exports = function (grunt) {
                 }
             }
         },
-        cssmin: {
-            minify: {
-                files: {
-                    'dist/neo-light.css': ['dist/neo-light.css'],
-                    'dist/neo-dark.css': ['dist/neo-dark.css']
-                }
-            }
-        },
-        imagemin: {
-            dynamic: {
-                options: {
-                    svgoPlugins: [{removeViewBox: true}]
-                },
-                files: [{
-                    expand: true,
-                    cwd: 'images/',
-                    src: ['**/*.svg'],
-                    dest: 'images/'
-                }]
-            }
+
+        fileExists: {
+            scripts: Object.keys(lessFiles)
         }
+
     });
 
     // Default task(s).
-    grunt.registerTask('default', ['clean', 'less', 'imageEmbed', 'cssmin']);
+    grunt.registerTask('default', ['clean', 'imagemin', 'less', 'replace', 'cssmin', 'postcss']);
+    grunt.registerTask('test', ['default', 'fileExists']);
+
+
 };
